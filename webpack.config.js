ae59d1af9ae188d4bbb1d128f15f0b22b3e5b3ab
Lemon23 <L_Qiao23@163.com>
2016-07-19 11:46:35 +0800

    config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    contentBase: path.resolve(__dirname, 'build')
+  }, 
+  module: {
+    loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015','react']
+      },
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css'
+    }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
